ff2ec78c6e0d18c1f88db754c71188b2b9f088c0
I Gusti Ngurah Adhya Pradipta <pradiptaadhya@gmail.com>
2023-11-16 15:19:26 +0700

    pertemuan tgl 16 Nop

--- routes/web.php
+++ routes/web.php
@@ -76,3 +76,21 @@ Route::get('style', function () {
 
 Route::get('perkalian', 'App\Http\Controllers\DosenController@index');
 Route::get('biodata', 'App\Http\Controllers\DosenController@biodata');
+
+Route::get('showjam/{jam}', 'App\Http\Controllers\DosenController@showtime');
+
+Route::get('formulir', 'App\Http\Controllers\DosenController@formulir');
+Route::post('/formulir/proses', 'App\Http\Controllers\DosenController@proses');
+
+Route::get('blogmaster', function () {
+    return view('home');
+});
+
+Route::get('/blogmaster/tentang', function () {
+    return view('tentang');
+});
+
+Route::get('/blogmaster/kontak', function () {
+    return view('kontak');
+});
+
